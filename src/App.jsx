ab40f19826d53103c66003ee79e5d6a146a2c767
k0d3x 8its <k0d3x8its@gmail.com>
2023-11-15 23:04:51 -0500

    centered title with tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import ComingSoon from './ComingSoon'
 
 const App = () => {
   return (
-    <div id="root" className="bg-gray-700 h-screen">
-      <h1 className="text-6xl text-center font-bold underline">Snowbits</h1>
+    <div id="root" className="bg-gray-700 h-screen text center">
+      <h1 className="text-6xl font-bold underline flex items-center justify-center h-screen pb-96">Snowbits</h1>
       <ComingSoon />
     </div>
   )

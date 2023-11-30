10a6389b832947d4a834e706009e015f8a1bb09a
k0d3x 8its <k0d3x8its@gmail.com>
2023-11-29 22:17:39 -0500

    added flex display and direction

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import ComingSoon from './Components/ComingSoon'
-import Footer from './Components/Footer'
+
 
 const App = () => {
+
   return (
-    <div className="bg-gray-700 h-screen text center">
+    <div className="bg-gray-700 h-screen text center flex flex-col">
       <h1 className="text-6xl font-bold underline flex items-center justify-center h-screen pb-96">Snowbits</h1>
       <ComingSoon />
     </div>

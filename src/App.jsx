c49a870b7b9306523c1134f7c29348ffb248d7c5
k0d3x 8its <k0d3x8its@gmail.com>
2023-12-14 21:37:12 -0500

    added the snowflake-circuit PNG

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import './App.css'
 import ComingSoon from './Components/ComingSoon'
+import avaxflakeCircuit from './assets/avaxflakeCircuit.png'
+
 
 
 const App = () => {
 
   return (
     <div className="bg-gray-700 h-screen text center flex flex-col">
-      <h1 className="text-6xl font-bold underline flex items-center justify-center h-screen pb-96">Snowbits</h1>
+      <h1 className="font-bold text-6xl flex item-center justify-center">
+          <span className="text-white pt-14 pl-4">
+            <u>Snow</u>
+          </span>
+          <span className="text-avax-red pt-14">
+            <u>Bits</u>
+          </span>
+      </h1>
+      <div>
+        <img 
+          src={avaxflakeCircuit} 
+          alt="avaxflake cicruit"
+          className="object-contain h-96 w-96 mx-auto" 
+        />
+      </div>
       <ComingSoon />
     </div>
+    
   )
 }
 

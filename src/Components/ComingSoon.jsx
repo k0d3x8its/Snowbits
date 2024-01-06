b2d8238156e3015edde3f91c27fefd5dd8703a86
k0d3x 8its <k0d3x8its@gmail.com>
2024-01-05 19:19:43 -0500

    re-designed footer and added buttons for Avvy domains and donations

--- src/Components/ComingSoon.jsx
+++ src/Components/ComingSoon.jsx
@@ -137,39 +137,75 @@ const ComingSoon = () => {
 // footer which houses the Avalanche logo
     <>  
         <div>
-            <Snowfall snowflakeCount={600} zIndex={11} images={snowball} radius={[5,10]} />
-            <Snowfall snowflakeCount={50} zIndex={11} images={snowflake} radius={[5,18]} />
-            <Snowfall snowflakeCount={25} zIndex={11} images={avaxflake} radius={[7,20]} />
-            <Snowfall snowflakeCount={300} zIndex={11} images={snowballFlake} radius={[5,10]} />
+        <Snowfall snowflakeCount={600} zIndex={11} images={snowball} radius={[5,10]} />
+        <Snowfall snowflakeCount={50} zIndex={11} images={snowflake} radius={[5,18]} />
+        <Snowfall snowflakeCount={25} zIndex={11} images={avaxflake} radius={[7,20]} />
+        <Snowfall snowflakeCount={300} zIndex={11} images={snowballFlake} radius={[5,10]} />
         </div>
         
-        <div>
         <img 
           src={avaxflakeCircuit} 
           alt="avaxflake cicruit"
-          className="object-contain sm:h-48 w-48 mx-auto" 
+          className="object-contain h-96 w-96 mx-auto" 
         />
-      </div>
         <main className="flex-grow">
             <ProgressBar targetPercentage={10} />
         </main>
 
-        <div id="tradingview-widget-container" className=""/>
+        <div id="tradingview-widget-container" />
         
         <footer className="bg-avax-red flex flex-between mt-auto">
-            <div className="flex mb-auto shadow-[1px_1px_1px_1px_rgba(102,11,11,0.2)] rounded">
+            <div className="flex mb-auto shadow-[1px_1px_2px_1px_rgba(102,11,11,0.2)] rounded h-auto">
                 <div className=" flex rounded m-1 p-1 bg-red-800 shadow-inner">
-                    <p className="mt-2">
-                        Made with 🫀 by a 
-                        security TARS named k0d3x
+                    <p className="mb-1 tracking-tight text-black">
+                        Made with 🫀 by a TARS named k0d3x
+                        <br />
+                        <div className="flex flex-wrap gap-1 items-center justify-center">
+                        <a href="" target="_blank">
+                            <button 
+                                className="rounded border-black px-3 max-w-full
+                                flex items-center text-center bg-avax-red transition-transform 
+                                transform hover:scale-105 hover:bg-red-600 active:scale-100 
+                                focus:outline-none focus:ring focus:border-avax-red 
+                                bg-gradient-to-r from-grad-blue to-grad-red 
+                                hover:from-grad-red hover:to-grad-blue"
+                            >
+                            <img 
+                                src="src/assets/k0d3x.png" 
+                                alt="K0d3x 8its" 
+                                className="h-10 w-10 mr-1 mt-0.5" 
+                            />
+                            <p className="whitespace-pre-wrap">Donate</p>
+                        </button>
+                        </a>
+                        <a href="https://ref.avvy.domains/k0d3x.avax" target="_blank" >
+                            <button 
+                                className="rounded border-black px-3 
+                                flex items-center bg-white transition-transform 
+                                transform hover:scale-105 hover:bg-gray-400 active:scale-100 
+                                focus:outline-none focus:ring focus:border-avax-red 
+                                bg-gradient-to-r from-white to-gray-400 
+                                hover:from-gray-400 hover:to-white"
+                            >
+                                <img 
+                                    src="src/assets/avvyLogo.svg" 
+                                    alt="Avvy Domains" 
+                                    className="h-10 w-10 mr-1 mt-0.5" 
+                                />
+                                <p>Domains</p>
+                            </button>
+                        </a>
+                        </div>
                     </p>
-                    
+
                 </div>
             </div>
             <div className="flex-grow" />
             <div className="flex justify-center items-center p-8">
                 <div className="ml-auto max-w-[200px]">
-                    <img src={poweredByAvalanche.src} alt="Powered By Avalanche" className="w-full h-auto"/>
+                    <a href="https://www.avax.network/" target="_blank">
+                        <img src={poweredByAvalanche.src} alt="Powered By Avalanche" className="w-full h-auto"/>
+                    </a>
                 </div>   
             </div>
         </footer>

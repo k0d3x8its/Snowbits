db58b4e12bd2270da2a61cbf17b3730a083787b2
K0d3x 8its <k0d3x@pm.me>
2025-11-12 10:34:56 -0500

    added import resolver for aliases

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,37 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
+
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
   ],
+
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+
+  settings: {
+    react: { version: '18.2' },
+    "import/resolver": {
+      alias: {
+        map: [
+          ["@assets", "./src/assets/"],
+        ],
+        extensions: [
+          ".js", ".jsx", ".ts", ".tsx", ".json", ".svg", ".png",
+          ".jpg", ".jpeg", ".gif", ".webp"
+        ],
+      },
+    },
+  },
+
+  plugins: ['react-refresh', 'import'],
+
   rules: {
     'react-refresh/only-export-components': [
       'warn',
